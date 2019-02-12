0a51430ec185abe41986ff5433d75d52c486a372
Reza <rezaisma932@gmail.com>
2019-02-12 10:57:28 +0700

    actionbar

---
+++ app/src/main/java/com/example/reza/form/TanggalSurveyArea.java
@@ -0,0 +1,14 @@
+package com.example.reza.form;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class TanggalSurveyArea extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tanggal_survey_area);
+    }
+}
