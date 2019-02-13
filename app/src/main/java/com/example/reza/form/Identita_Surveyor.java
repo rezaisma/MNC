346dde839897d4b753a8db1c294bd30228ef8369
Reza <rezaisma932@gmail.com>
2019-02-13 10:20:18 +0700

    layout user finish

---
+++ app/src/main/java/com/example/reza/form/Identita_Surveyor.java
@@ -0,0 +1,13 @@
+package com.example.reza.form;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Identita_Surveyor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_identita__surveyor);
+    }
+}
