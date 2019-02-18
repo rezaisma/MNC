dd9dda0e33d822082993371fb0eeeffe2904453f
Howler Nouvelle <hilmy@wahidin.net>
2019-02-18 12:05:19 +0700

    error messages REMASTERED (gak bisa pindah kalo blm diisi) + nge-set array di spinner kab/kota

--- app/src/main/java/com/example/reza/form/InfoKabKota.java
+++ app/src/main/java/com/example/reza/form/InfoKabKota.java
@@ -3,6 +3,7 @@ package com.example.reza.form;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -40,30 +41,27 @@ public class InfoKabKota extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_kab_kota);
         ButterKnife.bind(this);
+
     }
 
     @OnClick({R.id.btnNextKab, R.id.btnBackKab, R.id.edKecamtan, R.id.edKelurahan, R.id.edKompleks, R.id.edDeveloper})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.btnNextKab:
-                if (edKecamtan.length() == 0)
-                    edKecamtan.setError("Harus Diisi"
-                    );
-                else if (edKelurahan.length() == 0)
-                    edKelurahan.setError("Harus Diisi"
-                    );
-                else if (edKompleks.length() == 0)
-                    edKompleks.setError("Harus Diisi"
-                    );
-                else if (edDeveloper.length() == 0)
-                    edDeveloper.setError("Harus Diisi"
-                    );
+                if (TextUtils.isEmpty(edKecamtan.getText())) {
+                    edKecamtan.setError("Harus Diisi");
 
+                }if (TextUtils.isEmpty(edKelurahan.getText())) {
+                    edKelurahan.setError("Harus Diisi");
 
-                Intent intent4 = new Intent(this, InformasiDetailSurveyArea.class);
-                startActivity(intent4);
+                }if (TextUtils.isEmpty(edDeveloper.getText())) {
+                    edDeveloper.setError("Harus Diisi");
+
+                }else {
+                    Intent intent4 = new Intent(this, InformasiDetailSurveyArea.class);
+                    startActivity(intent4);
+                };
                 break;
-
             case R.id.btnBackKab:
                 Intent intent5 = new Intent(this, TanggalSurveyArea.class);
                 startActivity(intent5);
