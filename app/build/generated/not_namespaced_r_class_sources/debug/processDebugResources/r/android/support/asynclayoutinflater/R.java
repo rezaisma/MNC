6eb92192faa737e0ded0addec8c1f90056e25a87
Reza <rezaisma932@gmail.com>
2019-02-19 11:30:58 +0700

    add

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/asynclayoutinflater/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/asynclayoutinflater/R.java
@@ -78,34 +78,34 @@ public final class R {
     public static final class id {
         private id() {}
 
-        public static final int action_container = 0x7f090010;
-        public static final int action_divider = 0x7f090012;
-        public static final int action_image = 0x7f090013;
-        public static final int action_text = 0x7f090019;
-        public static final int actions = 0x7f09001a;
-        public static final int async = 0x7f090021;
-        public static final int blocking = 0x7f090025;
-        public static final int chronometer = 0x7f090053;
-        public static final int forever = 0x7f09008c;
-        public static final int icon = 0x7f090093;
-        public static final int icon_group = 0x7f090094;
-        public static final int info = 0x7f090099;
-        public static final int italic = 0x7f09009b;
-        public static final int line1 = 0x7f0900a0;
-        public static final int line3 = 0x7f0900a1;
-        public static final int normal = 0x7f0900ae;
-        public static final int notification_background = 0x7f0900af;
-        public static final int notification_main_column = 0x7f0900b0;
-        public static final int notification_main_column_container = 0x7f0900b1;
-        public static final int right_icon = 0x7f0900c3;
-        public static final int right_side = 0x7f0900c4;
-        public static final int tag_transition_group = 0x7f0900f4;
-        public static final int tag_unhandled_key_event_manager = 0x7f0900f5;
-        public static final int tag_unhandled_key_listeners = 0x7f0900f6;
-        public static final int text = 0x7f0900f7;
-        public static final int text2 = 0x7f0900f8;
-        public static final int time = 0x7f090100;
-        public static final int title = 0x7f090101;
+        public static final int action_container = 0x7f090011;
+        public static final int action_divider = 0x7f090013;
+        public static final int action_image = 0x7f090014;
+        public static final int action_text = 0x7f09001a;
+        public static final int actions = 0x7f09001b;
+        public static final int async = 0x7f090022;
+        public static final int blocking = 0x7f090026;
+        public static final int chronometer = 0x7f090054;
+        public static final int forever = 0x7f090093;
+        public static final int icon = 0x7f09009a;
+        public static final int icon_group = 0x7f09009b;
+        public static final int info = 0x7f0900a0;
+        public static final int italic = 0x7f0900a2;
+        public static final int line1 = 0x7f0900a7;
+        public static final int line3 = 0x7f0900a8;
+        public static final int normal = 0x7f0900b5;
+        public static final int notification_background = 0x7f0900b6;
+        public static final int notification_main_column = 0x7f0900b7;
+        public static final int notification_main_column_container = 0x7f0900b8;
+        public static final int right_icon = 0x7f0900ca;
+        public static final int right_side = 0x7f0900cb;
+        public static final int tag_transition_group = 0x7f0900fb;
+        public static final int tag_unhandled_key_event_manager = 0x7f0900fc;
+        public static final int tag_unhandled_key_listeners = 0x7f0900fd;
+        public static final int text = 0x7f0900fe;
+        public static final int text2 = 0x7f0900ff;
+        public static final int time = 0x7f090107;
+        public static final int title = 0x7f090108;
     }
     public static final class integer {
         private integer() {}
--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -73,20 +73,20 @@ public final class R {
     public static final class id {
         private id() {}
 
-        public static final int bottom = 0x7f090026;
-        public static final int end = 0x7f090072;
-        public static final int gone = 0x7f09008e;
-        public static final int invisible = 0x7f09009a;
-        public static final int left = 0x7f09009f;
-        public static final int packed = 0x7f0900b3;
-        public static final int parent = 0x7f0900b5;
-        public static final int percent = 0x7f0900b9;
-        public static final int right = 0x7f0900c2;
-        public static final int spread = 0x7f0900e9;
-        public static final int spread_inside = 0x7f0900ea;
-        public static final int start = 0x7f0900ef;
-        public static final int top = 0x7f090104;
-        public static final int wrap = 0x7f090115;
+        public static final int bottom = 0x7f090027;
+        public static final int end = 0x7f090079;
+        public static final int gone = 0x7f090095;
+        public static final int invisible = 0x7f0900a1;
+        public static final int left = 0x7f0900a6;
+        public static final int packed = 0x7f0900ba;
+        public static final int parent = 0x7f0900bc;
+        public static final int percent = 0x7f0900c0;
+        public static final int right = 0x7f0900c9;
+        public static final int spread = 0x7f0900f0;
+        public static final int spread_inside = 0x7f0900f1;
+        public static final int start = 0x7f0900f6;
+        public static final int top = 0x7f09010b;
+        public static final int wrap = 0x7f09011c;
     }
     public static final class styleable {
         private styleable() {}
