2391c95428f0aef261d2b69a8b3328a830f9736c
Howler Nouvelle <hilmy@wahidin.net>
2019-02-20 16:54:51 +0700

    retard

--- app/src/main/java/com/example/reza/form/TanggalSurveyArea.java
+++ app/src/main/java/com/example/reza/form/TanggalSurveyArea.java
@@ -1,25 +1,23 @@
 package com.example.reza.form;
 
-import android.app.DatePickerDialog;
+
 import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.Spinner;
 import android.widget.TextView;
-
-import java.util.Calendar;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class TanggalSurveyArea extends AppCompatActivity {
+public class TanggalSurveyArea extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
 
     @BindView(R.id.spinerRegion)
@@ -27,25 +25,48 @@ public class TanggalSurveyArea extends AppCompatActivity {
     @BindView(R.id.btnNextTanggal)
     Button btnNextTanggal;
 
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tanggal_survey_area);
         ButterKnife.bind(this);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.str_region, android.R.layout.simple_spinner_item);
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, new String[]{""});
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinerRegion.setAdapter(adapter);
+        spinerRegion.setOnItemSelectedListener(this);
 
     }
 
-    @OnClick(R.id.btnNextTanggal)
-    public void onViewClicked() {
-        Intent intent3 = new Intent(this, InfoKabKota.class);
-        startActivity(intent3);
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+//        ((TextView)spinerRegion.getSelectedView()).setError("Error message");
+        View selectedView = spinerRegion.getSelectedView();
+         if (selectedView != null && selectedView instanceof TextView);
+            spinerRegion.requestFocus();
+            TextView selectedTextView = (TextView) selectedView;
+            selectedTextView.setError("error");
+//            selectedTextView.setTextColor(Color.RED);
+//            selectedTextView.setText(error);
+            spinerRegion.performClick();
+    }
 
-
-
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
+
+    @OnClick({ R.id.btnNextTanggal})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btnNextTanggal:
+                Intent intent3 = new Intent(this, InfoKabKota.class);
+                startActivity(intent3);
+                break;
+        }
+    }
+
 }
