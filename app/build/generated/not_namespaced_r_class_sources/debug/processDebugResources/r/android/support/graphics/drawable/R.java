0e54d6129c5c42e29c192c2dcd1ffaf0be9a7a51
Reza <rezaisma932@gmail.com>
2019-02-20 16:23:10 +0700

    add

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/graphics/drawable/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/graphics/drawable/R.java
@@ -52,37 +52,37 @@ public final class R {
         public static final int compat_control_corner_material = 0x7f070054;
         public static final int compat_notification_large_icon_max_height = 0x7f070055;
         public static final int compat_notification_large_icon_max_width = 0x7f070056;
-        public static final int notification_action_icon_size = 0x7f0700c2;
-        public static final int notification_action_text_size = 0x7f0700c3;
-        public static final int notification_big_circle_margin = 0x7f0700c4;
-        public static final int notification_content_margin_start = 0x7f0700c5;
-        public static final int notification_large_icon_height = 0x7f0700c6;
-        public static final int notification_large_icon_width = 0x7f0700c7;
-        public static final int notification_main_column_padding_top = 0x7f0700c8;
-        public static final int notification_media_narrow_margin = 0x7f0700c9;
-        public static final int notification_right_icon_size = 0x7f0700ca;
-        public static final int notification_right_side_padding_top = 0x7f0700cb;
-        public static final int notification_small_icon_background_padding = 0x7f0700cc;
-        public static final int notification_small_icon_size_as_large = 0x7f0700cd;
-        public static final int notification_subtext_size = 0x7f0700ce;
-        public static final int notification_top_pad = 0x7f0700cf;
-        public static final int notification_top_pad_large_text = 0x7f0700d0;
+        public static final int notification_action_icon_size = 0x7f0700c3;
+        public static final int notification_action_text_size = 0x7f0700c4;
+        public static final int notification_big_circle_margin = 0x7f0700c5;
+        public static final int notification_content_margin_start = 0x7f0700c6;
+        public static final int notification_large_icon_height = 0x7f0700c7;
+        public static final int notification_large_icon_width = 0x7f0700c8;
+        public static final int notification_main_column_padding_top = 0x7f0700c9;
+        public static final int notification_media_narrow_margin = 0x7f0700ca;
+        public static final int notification_right_icon_size = 0x7f0700cb;
+        public static final int notification_right_side_padding_top = 0x7f0700cc;
+        public static final int notification_small_icon_background_padding = 0x7f0700cd;
+        public static final int notification_small_icon_size_as_large = 0x7f0700ce;
+        public static final int notification_subtext_size = 0x7f0700cf;
+        public static final int notification_top_pad = 0x7f0700d0;
+        public static final int notification_top_pad_large_text = 0x7f0700d1;
     }
     public static final class drawable {
         private drawable() {}
 
-        public static final int notification_action_background = 0x7f08006e;
-        public static final int notification_bg = 0x7f08006f;
-        public static final int notification_bg_low = 0x7f080070;
-        public static final int notification_bg_low_normal = 0x7f080071;
-        public static final int notification_bg_low_pressed = 0x7f080072;
-        public static final int notification_bg_normal = 0x7f080073;
-        public static final int notification_bg_normal_pressed = 0x7f080074;
-        public static final int notification_icon_background = 0x7f080075;
-        public static final int notification_template_icon_bg = 0x7f080076;
-        public static final int notification_template_icon_low_bg = 0x7f080077;
-        public static final int notification_tile_bg = 0x7f080078;
-        public static final int notify_panel_notification_icon_bg = 0x7f080079;
+        public static final int notification_action_background = 0x7f080070;
+        public static final int notification_bg = 0x7f080071;
+        public static final int notification_bg_low = 0x7f080072;
+        public static final int notification_bg_low_normal = 0x7f080073;
+        public static final int notification_bg_low_pressed = 0x7f080074;
+        public static final int notification_bg_normal = 0x7f080075;
+        public static final int notification_bg_normal_pressed = 0x7f080076;
+        public static final int notification_icon_background = 0x7f080077;
+        public static final int notification_template_icon_bg = 0x7f080078;
+        public static final int notification_template_icon_low_bg = 0x7f080079;
+        public static final int notification_tile_bg = 0x7f08007a;
+        public static final int notify_panel_notification_icon_bg = 0x7f08007b;
     }
     public static final class id {
         private id() {}
@@ -95,33 +95,33 @@ public final class R {
         public static final int async = 0x7f090021;
         public static final int blocking = 0x7f090025;
         public static final int bottom = 0x7f090026;
-        public static final int chronometer = 0x7f090053;
-        public static final int end = 0x7f090077;
-        public static final int forever = 0x7f090090;
-        public static final int icon = 0x7f090097;
-        public static final int icon_group = 0x7f090098;
-        public static final int info = 0x7f09009d;
-        public static final int italic = 0x7f09009f;
-        public static final int left = 0x7f0900a3;
-        public static final int line1 = 0x7f0900a4;
-        public static final int line3 = 0x7f0900a5;
-        public static final int none = 0x7f0900b1;
-        public static final int normal = 0x7f0900b2;
-        public static final int notification_background = 0x7f0900b3;
-        public static final int notification_main_column = 0x7f0900b4;
-        public static final int notification_main_column_container = 0x7f0900b5;
-        public static final int right = 0x7f0900c6;
-        public static final int right_icon = 0x7f0900c7;
-        public static final int right_side = 0x7f0900c8;
-        public static final int start = 0x7f0900f3;
-        public static final int tag_transition_group = 0x7f0900f8;
-        public static final int tag_unhandled_key_event_manager = 0x7f0900f9;
-        public static final int tag_unhandled_key_listeners = 0x7f0900fa;
-        public static final int text = 0x7f0900fb;
-        public static final int text2 = 0x7f0900fc;
-        public static final int time = 0x7f090104;
-        public static final int title = 0x7f090105;
-        public static final int top = 0x7f090108;
+        public static final int chronometer = 0x7f090055;
+        public static final int end = 0x7f090079;
+        public static final int forever = 0x7f090092;
+        public static final int icon = 0x7f090099;
+        public static final int icon_group = 0x7f09009a;
+        public static final int info = 0x7f09009f;
+        public static final int italic = 0x7f0900a1;
+        public static final int left = 0x7f0900a6;
+        public static final int line1 = 0x7f0900a7;
+        public static final int line3 = 0x7f0900a8;
+        public static final int none = 0x7f0900b4;
+        public static final int normal = 0x7f0900b5;
+        public static final int notification_background = 0x7f0900b6;
+        public static final int notification_main_column = 0x7f0900b7;
+        public static final int notification_main_column_container = 0x7f0900b8;
+        public static final int right = 0x7f0900c9;
+        public static final int right_icon = 0x7f0900ca;
+        public static final int right_side = 0x7f0900cb;
+        public static final int start = 0x7f0900f6;
+        public static final int tag_transition_group = 0x7f0900fb;
+        public static final int tag_unhandled_key_event_manager = 0x7f0900fc;
+        public static final int tag_unhandled_key_listeners = 0x7f0900fd;
+        public static final int text = 0x7f0900fe;
+        public static final int text2 = 0x7f0900ff;
+        public static final int time = 0x7f090107;
+        public static final int title = 0x7f090108;
+        public static final int top = 0x7f09010b;
     }
     public static final class integer {
         private integer() {}
@@ -141,19 +141,19 @@ public final class R {
     public static final class string {
         private string() {}
 
-        public static final int status_bar_notification_info_overflow = 0x7f0e0043;
+        public static final int status_bar_notification_info_overflow = 0x7f0f0043;
     }
     public static final class style {
         private style() {}
 
-        public static final int TextAppearance_Compat_Notification = 0x7f0f0115;
-        public static final int TextAppearance_Compat_Notification_Info = 0x7f0f0116;
-        public static final int TextAppearance_Compat_Notification_Line2 = 0x7f0f0117;
-        public static final int TextAppearance_Compat_Notification_Time = 0x7f0f0118;
-        public static final int TextAppearance_Compat_Notification_Title = 0x7f0f0119;
-        public static final int Widget_Compat_NotificationActionContainer = 0x7f0f01bf;
-        public static final int Widget_Compat_NotificationActionText = 0x7f0f01c0;
-        public static final int Widget_Support_CoordinatorLayout = 0x7f0f01ef;
+        public static final int TextAppearance_Compat_Notification = 0x7f100118;
+        public static final int TextAppearance_Compat_Notification_Info = 0x7f100119;
+        public static final int TextAppearance_Compat_Notification_Line2 = 0x7f10011a;
+        public static final int TextAppearance_Compat_Notification_Time = 0x7f10011b;
+        public static final int TextAppearance_Compat_Notification_Title = 0x7f10011c;
+        public static final int Widget_Compat_NotificationActionContainer = 0x7f1001c2;
+        public static final int Widget_Compat_NotificationActionText = 0x7f1001c3;
+        public static final int Widget_Support_CoordinatorLayout = 0x7f1001f2;
     }
     public static final class styleable {
         private styleable() {}
--- app/src/main/java/com/example/reza/form/Identita_Surveyor.java
+++ app/src/main/java/com/example/reza/form/Identita_Surveyor.java
@@ -1,22 +1,35 @@
 package com.example.reza.form;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.reza.form.loginactivity.LoginAdminActivity;
+import java.io.IOException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class Identita_Surveyor extends AppCompatActivity {
+
+    private final int REQUEST_IMAGE_CAPTURE = 1, REQUEST_IMAGE_GALLERY = 2;
+    private ImageView image;
 
     @BindView(R.id.rbYa)
     RadioButton rbYa;
@@ -27,16 +40,75 @@ public class Identita_Surveyor extends AppCompatActivity {
     @BindView(R.id.btnBackIdentitas)
     Button btnBackIdentitas;
 
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example_menu, menu);
+        return true;
+    }
+
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.btnCamera:
+                Intent iCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if(iCamera.resolveActivity(getPackageManager()) != null){
+                    startActivityForResult(iCamera,REQUEST_IMAGE_CAPTURE);
+                }
+                break;
+            case R.id.btnGallery:
+                Intent iGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                iGallery.setType("image/*");
+                startActivityForResult(iGallery, REQUEST_IMAGE_GALLERY);
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_IMAGE_CAPTURE){
+                Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                image.setImageBitmap(bitmap);
+            }else if (requestCode == REQUEST_IMAGE_GALLERY){
+                Uri uri = data.getData();
+                Bitmap bitmap = null;
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                    image.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_identita__surveyor);
         ButterKnife.bind(this);
+
+        image = (ImageView)findViewById(R.id.ivIdentitas);
+
+
+
     }
 
     @OnClick({R.id.btnFinish, R.id.btnBackIdentitas})
     public void onViewClicked(View view) {
         switch (view.getId()) {
+
             case R.id.btnFinish:
                 break;
             case R.id.btnBackIdentitas:
