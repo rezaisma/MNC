3dd1b984c8e7b0feb96014d645851c4cfda3a247
Reza <rezaisma932@gmail.com>
2019-03-04 11:49:27 +0700

    set error chechbox

--- app/src/main/java/com/example/reza/form/InformasiDetailSurveyArea.java
+++ app/src/main/java/com/example/reza/form/InformasiDetailSurveyArea.java
@@ -24,14 +24,22 @@ public class InformasiDetailSurveyArea extends AppCompatActivity {
 
     //TODO CHECKBOX GROUP
      CheckBox CBAjenisPro, CBBjenisPro, CBCjenisPro,
-              CBAklartipe, CBBklartipe, CBCklartipe, CBDklartipe, CBEklartipe, CBFklartipe;
+              CBAklartipe, CBBklartipe, CBCklartipe, CBDklartipe, CBEklartipe, CBFklartipe,
+              CBAMetod, CBBMetod, CBCMetod, CBDMetod,
+              CBAAkses, CBBAkses, CBCAkses, CBDAkses,
+              CBAKom, CBBKom, CBCKom, CBDKom, CBEKom, CBFKom,
+              CBAPro, CBBPro, CBCPro, CBDPro, CBEPro, CBFPro;
 
     //TODO BUTTON GROUP
      Button btnNDS, btnBDS;
 
     //TODO STRING GROUP
      String s_cbajenispro, s_cbbjenispro, s_cbcjenispro,
-            s_aklartipe, s_bklartipe, s_cklartipe, s_dklartipe, s_eklartipe, s_fklartipe;
+            s_aklartipe, s_bklartipe, s_cklartipe, s_dklartipe, s_eklartipe, s_fklartipe,
+            s_cbametode, s_cbbmetode, s_cbcmetode, s_cbdmetode,
+            s_cbaakses, s_cbbakses, s_cbcakses, s_cbdakses,
+            s_cbakom, s_cbbkom, s_cbckom, s_cbdkom, s_cbekom, s_cbfkom,
+            s_cbapro, s_cbbpro, s_cbcpro, s_cbdpro, s_cbepro, s_cbfpro;
 
 
 
@@ -149,25 +157,85 @@ public class InformasiDetailSurveyArea extends AppCompatActivity {
         CBEklartipe =  findViewById(R.id.cbEKlarifikasiTipe);
         CBFklartipe =  findViewById(R.id.cbFKlarifikasiTipe);
 
+        //TODO METODE PEMBANGUNAN
+        CBAMetod = findViewById(R.id.cbAMetode);
+        CBBMetod = findViewById(R.id.cbBMetode);
+        CBCMetod = findViewById(R.id.cbCMetode);
+        CBDMetod = findViewById(R.id.cbDMetode);
+
+        //TODO AKSES PENJUALAN
+        CBAAkses = findViewById(R.id.cbAAkses);
+        CBBAkses = findViewById(R.id.cbBAkses);
+        CBCAkses = findViewById(R.id.cbCAkses);
+        CBDAkses = findViewById(R.id.cbDAkses);
+
+        //TODO KOMPETITOR
+        CBAKom = findViewById(R.id.cbAkompetitor);
+        CBBKom = findViewById(R.id.cbBkompetitor);
+        CBCKom = findViewById(R.id.cbCkompetitor);
+        CBDKom = findViewById(R.id.cbDkompetitor);
+        CBEKom = findViewById(R.id.cbEkompetitor);
+        CBFKom = findViewById(R.id.cbFKompetitor);
+
+        //TODO MAYORITAS PROVIDER
+        CBAPro = findViewById(R.id.cbAMayoritas);
+        CBBPro = findViewById(R.id.cbBMayoritas);
+        CBCPro = findViewById(R.id.cbCMayoritas);
+        CBDPro = findViewById(R.id.cbDMayoritas);
+        CBEPro = findViewById(R.id.cbEMayoritas);
+        CBFPro = findViewById(R.id.cbFMayoritas);
+
+
+
         //TODO BUTTON
         btnNDS =  findViewById(R.id.btnNextDetailSurvey);
         btnBDS =  findViewById(R.id.btnBackDetailSurvey);
 
 
 
-    //TODO STRING JENIS PROPERTI
-        s_cbajenispro="";
-        s_cbbjenispro="";
-        s_cbcjenispro="";
+            //TODO STRING JENIS PROPERTI
+            s_cbajenispro="";
+            s_cbbjenispro="";
+            s_cbcjenispro="";
 
 
-    //TODO STRING KLARIFIKASI TIPE
-        s_aklartipe="";
-        s_bklartipe="";
-        s_cklartipe="";
-        s_dklartipe="";
-        s_eklartipe="";
-        s_fklartipe="";
+                //TODO STRING KLARIFIKASI TIPE
+                s_aklartipe="";
+                s_bklartipe="";
+                s_cklartipe="";
+                s_dklartipe="";
+                s_eklartipe="";
+                s_fklartipe="";
+
+
+                    //TODO STRING METODE PEMBANGUNAN
+                    s_cbametode="";
+                    s_cbbmetode="";
+                    s_cbcmetode="";
+                    s_cbdmetode="";
+
+
+                        //TODO STRING AKSES PENJUALAN
+                        s_cbaakses="";
+                        s_cbbakses="";
+                        s_cbcakses="";
+                        s_cbdakses="";
+
+                            //TODO KOMPETITOR
+                            s_cbakom="";
+                            s_cbbkom="";
+                            s_cbckom="";
+                            s_cbdkom="";
+                            s_cbekom="";
+                            s_cbfkom="";
+
+                                //TODO MAYORITAS PROVIDER
+                                s_cbapro="";
+                                s_cbbpro="";
+                                s_cbcpro="";
+                                s_cbdpro="";
+                                s_cbepro="";
+                                s_cbfpro="";
 
 
         //TODO CHECKBOX JENIS PROPERTI
@@ -190,83 +258,266 @@ public class InformasiDetailSurveyArea extends AppCompatActivity {
         }
 
 
-        //TODO CHECKBOX KLARIFIKASI TIPE
-        if (CBAklartipe.isChecked()){
-            s_aklartipe=CBAklartipe.getText().toString();
+                //TODO CHECKBOX KLARIFIKASI TIPE
+                if (CBAklartipe.isChecked()){
+                    s_aklartipe=CBAklartipe.getText().toString();
+                } else {
+                    s_aklartipe="";
+                }
+
+                if (CBBklartipe.isChecked()){
+                    s_bklartipe=CBBklartipe.getText().toString();
+                } else {
+                    s_bklartipe="";
+                }
+
+                if (CBCklartipe.isChecked()){
+                    s_cklartipe=CBCklartipe.getText().toString();
+                } else {
+                    s_cklartipe="";
+                }
+
+                if (CBDklartipe.isChecked()){
+                    s_dklartipe=CBDklartipe.getText().toString();
+                } else {
+                    s_dklartipe="";
+                }
+
+                if (CBEklartipe.isChecked()){
+                    s_eklartipe=CBEklartipe.getText().toString();
+                } else {
+                    s_eklartipe="";
+                }
+
+                if (CBFklartipe.isChecked()){
+                    s_fklartipe=CBFklartipe.getText().toString();
+                } else {
+                    s_fklartipe="";
+                }
+
+
+                        //TODO CHECKBOX METODE PEMBANGUNAN
+                        if (CBAMetod.isChecked()){
+                            s_cbametode=CBAMetod.getText().toString();
+                        } else {
+                            s_cbametode="";
+                        }
+
+                        if (CBBMetod.isChecked()){
+                            s_cbbmetode=CBBMetod.getText().toString();
+                        } else {
+                            s_cbbmetode="";
+                        }
+
+                        if (CBCMetod.isChecked()){
+                            s_cbcmetode=CBCMetod.getText().toString();
+                        } else {
+                            s_cbcmetode="";
+                        }
+
+                        if (CBDMetod.isChecked()){
+                            s_cbdmetode=CBDMetod.getText().toString();
+                        } else {
+                            s_cbdmetode="";
+                        }
+
+
+                                //TODO CHECKBOX AKSES PENJUALAN
+                                if (CBAAkses.isChecked()){
+                                    s_cbaakses=CBAAkses.getText().toString();
+                                } else {
+                                    s_cbaakses="";
+                                }
+
+                                if (CBBAkses.isChecked()){
+                                    s_cbbakses=CBBAkses.getText().toString();
+                                } else {
+                                    s_cbbakses="";
+                                }
+
+                                if (CBCAkses.isChecked()){
+                                    s_cbcakses=CBCAkses.getText().toString();
+                                } else {
+                                    s_cbcakses="";
+                                }
+
+                                if (CBDAkses.isChecked()){
+                                    s_cbdakses=CBDAkses.getText().toString();
+                                } else {
+                                    s_cbdakses="";
+                                }
+
+
+                                        //TODO CHECKBOX KOMPETITOR
+                                        if (CBAKom.isChecked()){
+                                            s_cbakom=CBAKom.getText().toString();
+                                        } else {
+                                            s_cbakom="";
+                                        }
+
+                                        if (CBBKom.isChecked()){
+                                            s_cbbkom=CBBKom.getText().toString();
+                                        } else {
+                                            s_cbbkom="";
+                                        }
+
+                                        if (CBCKom.isChecked()){
+                                            s_cbckom=CBCKom.getText().toString();
+                                        } else {
+                                            s_cbckom="";
+                                        }
+
+                                        if (CBDKom.isChecked()){
+                                            s_cbdkom=CBDKom.getText().toString();
+                                        } else {
+                                            s_cbdkom="";
+                                        }
+
+                                        if (CBEKom.isChecked()){
+                                            s_cbekom=CBEKom.getText().toString();
+                                        } else {
+                                            s_cbekom="";
+                                        }
+
+                                        if (CBFKom.isChecked()){
+                                            s_cbfkom=CBFKom.getText().toString();
+                                        } else {
+                                            s_cbfkom="";
+                                        }
+
+
+        //TODO CHECKBOX KOMPETITOR
+        if (CBAPro.isChecked()){
+            s_cbapro=CBAPro.getText().toString();
         } else {
-            s_aklartipe="";
+            s_cbapro="";
         }
 
-        if (CBBklartipe.isChecked()){
-            s_bklartipe=CBBklartipe.getText().toString();
+        if (CBBPro.isChecked()){
+            s_cbbpro=CBBPro.getText().toString();
         } else {
-            s_bklartipe="";
+            s_cbbpro="";
         }
 
-        if (CBCklartipe.isChecked()){
-            s_cklartipe=CBCklartipe.getText().toString();
+        if (CBCPro.isChecked()){
+            s_cbcpro=CBCPro.getText().toString();
         } else {
-            s_cklartipe="";
+            s_cbcpro="";
         }
 
-        if (CBDklartipe.isChecked()){
-            s_dklartipe=CBDklartipe.getText().toString();
+        if (CBDPro.isChecked()){
+            s_cbdpro=CBDPro.getText().toString();
         } else {
-            s_dklartipe="";
+            s_cbdpro="";
         }
 
-        if (CBEklartipe.isChecked()){
-            s_eklartipe=CBEklartipe.getText().toString();
+        if (CBEPro.isChecked()){
+            s_cbepro=CBEPro.getText().toString();
         } else {
-            s_eklartipe="";
+            s_cbepro="";
         }
 
-        if (CBFklartipe.isChecked()){
-            s_fklartipe=CBFklartipe.getText().toString();
+        if (CBFPro.isChecked()){
+            s_cbfpro=CBFPro.getText().toString();
         } else {
-            s_fklartipe="";
+            s_cbfpro="";
         }
 
 
         //TODO BUTTON NEXT
-        btnNDS.setOnClickListener(new View.OnClickListener() {
+        btnNDS.setOnClickListener(
+                new View.OnClickListener() {
             @SuppressLint("ResourceType")
             @Override
             public void onClick(View v) {
 
-                if (!CBAjenisPro.isChecked() && !CBBjenisPro.isChecked() && !CBCjenisPro.isChecked()){
+                if (!CBAjenisPro.isChecked() && !CBBjenisPro.isChecked() && !CBCjenisPro.isChecked()) {
 
-                    CBAjenisPro.setError("isi data");
-                    CBBjenisPro.setError("isi data");
-                    CBCjenisPro.setError("isi data");
+                        CBAjenisPro.setError("isi data");
+                        CBBjenisPro.setError("isi data");
+                        CBCjenisPro.setError("isi data");
 
                     Toast.makeText(InformasiDetailSurveyArea.this, "Isi terlebih dahulu sebelum melanjutkat ke halaman berikutnya", Toast.LENGTH_SHORT).show();
 
-                if (!CBAklartipe.isChecked() && !CBBklartipe.isChecked() && !CBCklartipe.isChecked() && !CBDklartipe.isChecked() && !CBEklartipe.isChecked() && !CBFklartipe.isChecked())
+                    if (!CBAklartipe.isChecked() && !CBBklartipe.isChecked() && !CBCklartipe.isChecked() && !CBDklartipe.isChecked() && !CBEklartipe.isChecked() && !CBFklartipe.isChecked()) {
 
-                    CBAklartipe.setError("isi data");
-                    CBBklartipe.setError("isi data");
-                    CBCklartipe.setError("isi data");
-                    CBDklartipe.setError("isi data");
-                    CBEklartipe.setError("isi data");
-                    CBFklartipe.setError("isi data");
+                        CBAklartipe.setError("isi data");
+                        CBBklartipe.setError("isi data");
+                        CBCklartipe.setError("isi data");
+                        CBDklartipe.setError("isi data");
+                        CBEklartipe.setError("isi data");
+                        CBFklartipe.setError("isi data");
 
-                    Toast.makeText(InformasiDetailSurveyArea.this, "Isi data", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(InformasiDetailSurveyArea.this, "Isi terlebih dahulu sebelum melanjutkat ke halaman berikutnya", Toast.LENGTH_SHORT).show();
 
-                    if (TextUtils.isEmpty(edRata2BiayaInternet.getText())) {
-                        edRata2BiayaInternet.setError("Harus Diisi");
                     }
 
-                    if (Radiogroup.getCheckedRadioButtonId() <= 0) {
-                        int radioId = Radiogroup.getCheckedRadioButtonId();
-                        radioButton = findViewById(radioId);
-                        rbMotor.setError("Select Item");
+
+                    if (!CBAMetod.isChecked() && !CBBMetod.isChecked() && !CBCMetod.isChecked() && !CBDMetod.isChecked()) {
+
+                        CBAMetod.setError("isi data");
+                        CBBMetod.setError("isi data");
+                        CBCMetod.setError("isi data");
+                        CBDMetod.setError("isi data");
+
+                        Toast.makeText(InformasiDetailSurveyArea.this, "Isi terlebih dahulu sebelum melanjutkat ke halaman berikutnya", Toast.LENGTH_SHORT).show();
+
                     }
-                } else {
-                    Intent intentnext = new Intent(getApplicationContext(),Identita_Surveyor.class);
-                    startActivity(intentnext);
+
+
+                    if (!CBAAkses.isChecked() && !CBBAkses.isChecked() && !CBCAkses.isChecked() && !CBDAkses.isChecked()){
+
+                        CBAAkses.setError("isi data");
+                        CBBAkses.setError("isi data");
+                        CBCAkses.setError("isi data");
+                        CBDAkses.setError("isi data");
+
+                        Toast.makeText(InformasiDetailSurveyArea.this, "Isi terlebih dahulu sebelum melanjutkat ke halaman berikutnya", Toast.LENGTH_SHORT).show();
+
+                    }
+
+                    if (!CBAKom.isChecked() && !CBBKom.isChecked() && !CBCKom.isChecked() && !CBDKom.isChecked() && !CBEKom.isChecked() && !CBFKom.isChecked()){
+
+                        CBAKom.setError("isi data");
+                        CBBKom.setError("isi data");
+                        CBCKom.setError("isi data");
+                        CBDKom.setError("isi data");
+                        CBEKom.setError("isi data");
+                        CBFKom.setError("isi data");
+
+                        Toast.makeText(InformasiDetailSurveyArea.this, "Isi terlebih dahulu sebelum melanjutkat ke halaman berikutnya", Toast.LENGTH_SHORT).show();
+                    }
+
+                    if (!CBAPro.isChecked() && !CBBPro.isChecked() && !CBCPro.isChecked() && !CBDPro.isChecked() && !CBEPro.isChecked() && !CBFPro.isChecked()){
+
+                        CBAPro.setError("isi data");
+                        CBBPro.setError("isi data");
+                        CBCPro.setError("isi data");
+                        CBDPro.setError("isi data");
+                        CBEPro.setError("isi data");
+                        CBFPro.setError("isi data");
+
+                        Toast.makeText(InformasiDetailSurveyArea.this, "Isi terlebih dahulu sebelum melanjutkat ke halaman berikutnya" +
+                                "" +
+                                "", Toast.LENGTH_SHORT).show();
+                    }
+
+                        if (TextUtils.isEmpty(edRata2BiayaInternet.getText())) {
+                            edRata2BiayaInternet.setError("Harus Diisi");
+                        }
+
+                        if (Radiogroup.getCheckedRadioButtonId() <= 0) {
+                            int radioId = Radiogroup.getCheckedRadioButtonId();
+                            radioButton = findViewById(radioId);
+                            rbMotor.setError("Select Item");
+                        }
+                    } else {
+                        Intent intentnext = new Intent(getApplicationContext(), Identita_Surveyor.class);
+                        startActivity(intentnext);
+                    }
                 }
-            }
+
+
 
         });
 
